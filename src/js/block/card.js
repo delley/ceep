5e3e88b01dc95a939ab134364789a3009ba0312d
Francisco Oliveira <delley.fx@gmail.com>
2017-06-24 20:01:00 -0300

    Implementação da funcionalidade de remover cards

--- src/js/block/card.js
+++ src/js/block/card.js
@@ -1,3 +1,25 @@
-var $cardColors = document.querySelectorAll('.card-colors');
+var $cards = document.querySelectorAll('.card');
+var $cardColors =  document.querySelectorAll('.card-options');
 
-console.log($cardColors);
+for (var index = 0; index < $cards.length; index++) {
+  $cards[index].addEventListener('click', function(event) {
+    var $this = event.target;
+
+    var $card = this;
+
+    if ($this.dataset.color) {
+      $card.dataset.color = $this.dataset.color;
+
+      for (var position = 0; position < $cardColors.length; position++) {
+        $cardColors[position].classList.remove('isActive');
+      }
+
+      $this.classList.add('isActive');
+    }
+
+    if ($this.classList.contains('card_delete')) {
+      $card.remove();
+    }
+
+  });
+}
